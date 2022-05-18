33962a3b82ba983b5fb22ced0a8c3022ee302177
ASTELL <ASTELL@DESKTOP-QCP1NQK>
2022-05-18 12:00:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Lab3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var d1 = new Dictionary<string, int>()
+            {
+                ["a"] = 1,
+                ["b"] = 2,
+                ["c"] = 3,
+            };
+            foreach (KeyValuePair<string, int> letter in d1)
+            {
+                Console.WriteLine($" d1:  key: {letter.Key}  value: {letter.Value}");
+                
+            }
+            Console.WriteLine("--------------------");
+            var d2 = new Dictionary<string, int>()
+            {
+                ["a"] = 3,
+                ["b"] = 2,
+                ["d"] = 4,
+            };
+            foreach (KeyValuePair<string, int> letter in d2)
+            {
+                Console.WriteLine($" d2:  key: {letter.Key}  value: {letter.Value}");
+               
+            }
+            Console.WriteLine("--------------------");
+
+            var resDict = d1.Concat(d2).GroupBy(x => x.Key)
+                   .ToDictionary(x => x.Key, x => x.Sum(y => y.Value));
+            foreach (KeyValuePair<string, int> e in resDict)
+                {
+                    Console.WriteLine($" d1+d2:  key: {e.Key}  value: {e.Value}");
+                }
+         }
+
+    }
+}
